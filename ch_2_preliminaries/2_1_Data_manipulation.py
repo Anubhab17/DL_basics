ed67702bf1b2740d2369b0cbf8be385c615485d2
Anubhabsupsi <anubhab.biswas@supsi.ch>
2024-09-12 17:25:59 +0200

    Initialized 2_1_Data_manipulation.py

--- ch_2_preliminaries/2_1_Data_manipulation.py
+++ ch_2_preliminaries/2_1_Data_manipulation.py
@@ -36,3 +36,26 @@ print(X)
 X[:2, :] = 12
 print(X)
 
+# Operations
+
+print(torch.exp(x))
+
+x = torch.tensor([1.0, 2, 4, 8])
+y = torch.tensor([2, 2, 2, 2])
+print(x + y, x - y, x * y, x / y, x ** y)
+
+X = torch.arange(12, dtype=torch.float32).reshape((3,4))
+Y = torch.tensor([[2.0, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
+print(torch.cat((X, Y), dim=0), torch.cat((X, Y), dim=1))
+
+print(X == Y)
+
+print(X.sum())
+
+# Broadcasting
+
+a = torch.arange(3).reshape((3, 1))
+b = torch.arange(2).reshape((1, 2))
+print(a, b)
+print(a + b)
+
